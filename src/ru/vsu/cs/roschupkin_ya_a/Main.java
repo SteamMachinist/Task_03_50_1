957b32adccd4cd13a4aa6efddd488b6e26a56947
Yaroslav Roschupkin <yaroslav-r.01@mail.ru>
2020-10-18 13:20:07 +0300

    Added Picture class realization

--- src/ru/vsu/cs/roschupkin_ya_a/Main.java
+++ src/ru/vsu/cs/roschupkin_ya_a/Main.java
@@ -3,112 +3,90 @@ package ru.vsu.cs.roschupkin_ya_a;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
 
     public static void main(String[] args)
     {
         Locale.setDefault(Locale.ROOT);
 
-        printExamples();
-
-        double x = readCoordinate('X');
-        double y = readCoordinate('Y');
-        printColorForPoint(x, y, getColor(x, y));
-    }
-
-    static void printExamples()
-    {
         for (int i = 0; i < 11; i++)
         {
-            printColorForPoint(readExamplesX(i), readExamplesY(i),
-                    getColor(readExamplesX(i), readExamplesY(i)));
+            printColorForPoint(examplesX(i), examplesY(i));
         }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input X: ");
+        double x = sc.nextDouble();
+        if ((x < -10) || (x > 10))
+        {
+            System.out.println("-10 < x < 10");
+            System.exit(1);
+        }
+
+        System.out.print("Input Y: ");
+        double y = sc.nextDouble();
+        if ((y < -10) || (y > 10))
+        {
+            System.out.println("-10 < y < 10");
+            System.exit(1);
+        }
+
+        printColorForPoint(x, y);
     }
 
-    static double readExamplesX(int i)
+    static double examplesX(int i)
     {
         double [] xExamples = {0, 8, 3, 4, 6, -1, -3, -1.5 ,0, -3, -9};
         return xExamples[i];
     }
 
-    static double readExamplesY(int i)
+    static double examplesY(int i)
     {
         double [] yExamples = {0, 1, 3, 6, 3, 1, -1, 3.5 ,5, 2, -9};
         return yExamples[i];
     }
 
-    static void printColorForPoint (double x, double y, SimpleColor areaColor)
+    public static void printColorForPoint (double x, double y)
     {
-        System.out.printf("(%.3f ; %.3f) -> %s \n", x, y, areaColor);
+        System.out.printf("(%.3f ; %.3f) -> %s \n", x, y, getColor(x, y));
     }
 
-    static double readCoordinate(char coordinateName)
+    public static SimpleColor getColor (double x, double y)
     {
-        System.out.printf("Input %c: ", coordinateName);
-        Scanner sc = new Scanner(System.in);
-        return sc.nextDouble();
-    }
-
-    static SimpleColor getColor (double x, double y)
-    {
-        if (isColorOrange(x, y)) return SimpleColor.ORANGE;
-
         if (isColorWhite(x, y)) return SimpleColor.WHITE;
-
-        if (isColorBlue(x, y)) return SimpleColor.BLUE;
-
-        if (isColorYellow(x, y)) return SimpleColor.YELLOW;
-
-        if (isColorGray(x, y)) return SimpleColor.GRAY;
-
-        return SimpleColor.GREEN;
+        return SimpleColor.BLUE;
     }
 
-    public static final HorizontalParabola horizontalParabola = new HorizontalParabola(3, -1, 0.125);
-    public static final BigSquare bigSquare = new BigSquare (-2, -5, 6, 4);
-    public static final SmallSquare smallSquare = new SmallSquare(2, 2, 7, 7);
-    public static final BigParabola bigParabola = new BigParabola(-3, -3 , 0.5);
-    public static final SmallParabola smallParabola = new SmallParabola(-3, 0, 1);
-
-    static boolean isColorOrange (double x, double y)
+    public static boolean isColorWhite (double x, double y)
     {
-        return (smallParabola.isPointHigherSmallParabola(x, y) && !bigSquare.isPointInBigSquare(x, y))
-
-                || (horizontalParabola.isPointRightOfParabola(x, y) && !bigSquare.isPointInBigSquare(x, y)
-                && !smallSquare.isPointInSmallSquare(x, y));
+        return y >= (0.5 * (x + 3) * (x + 3) - 3) && (y <= (x + 3) * (x + 3))
+                && ((x >= -2) && (x <= 6) && (y >= -5) && (y <= 4))
+                || (x >= 2) && (x <= 7) && (y >= 2) && (y <= 7) && x >= (0.125 * (y + 1) * (y + 1) + 3);
     }
 
-    static boolean isColorWhite (double x, double y)
+    public static boolean isColorGray (double x, double y)
     {
-        return (bigParabola.isPointHigherBigParabola(x, y) && bigSquare.isPointInBigSquare(x, y) &&
-               !smallParabola.isPointHigherSmallParabola(x, y))
-
-                || (smallSquare.isPointInSmallSquare(x, y) && horizontalParabola.isPointRightOfParabola(x, y));
-
+        return true;
     }
 
-    static boolean isColorBlue (double x, double y)
+    public static boolean isColorOrange (double x, double y)
     {
-        return (bigParabola.isPointHigherBigParabola(x, y) && !smallParabola.isPointHigherSmallParabola(x, y)
-                && !bigSquare.isPointInBigSquare(x, y) && (x <= -2))
-
-                || (smallSquare.isPointInSmallSquare(x, y) && !horizontalParabola.isPointRightOfParabola(x, y)
-                && !bigSquare.isPointInBigSquare(x, y));
+        return true;
     }
 
-    static boolean isColorYellow (double x, double y)
+    public static boolean isColorYellow (double x, double y)
     {
-        return (bigParabola.isPointHigherBigParabola(x, y) && !smallParabola.isPointHigherSmallParabola(x, y)
-                && !bigSquare.isPointInBigSquare(x, y) && (x > -2))
-
-                || (smallSquare.isPointInSmallSquare(x, y) && !horizontalParabola.isPointRightOfParabola(x, y)
-                && bigSquare.isPointInBigSquare(x, y));
+        return true;
     }
 
-    static boolean isColorGray (double x, double y)
+    public static boolean isColorGreen (double x, double y)
     {
-        return (bigSquare.isPointInBigSquare(x, y) && !bigParabola.isPointHigherBigParabola(x, y)
-                && !smallSquare.isPointInSmallSquare(x, y));
+        return true;
+    }
+
+    public static boolean isColorBlue (double x, double y)
+    {
+        return true;
     }
 }
--- src/ru/vsu/cs/roschupkin_ya_a/Shape.java
+++
@@ -1,6 +0,0 @@
-package ru.vsu.cs.roschupkin_ya_a;
-
-public interface Shape
-{
-    boolean isPointInside(Point point);
-}
